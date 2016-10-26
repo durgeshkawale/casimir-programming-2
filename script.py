f4606ed037f9a7d76a7d4ac0194f667f5307cbfb
Durgesh Kawale <durgeshkawale@gmail.com>
2016-10-26 09:28:08 +0000

    script.py added, functions in test.py

---
+++ script.py
@@ -0,0 +1,10 @@
+import numpy as np
+import test as myfunc
+
+r = 1  # radius of my circle
+circum = myfunc.circumference(r)
+print("Radius of my circle is ", r, "and circumference is ", circum)
+print()
+SA = myfunc.SurfArea(r)
+print("Surface area is ", SA)
+print()
--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 print('hello world')
 import numpy as np
-r = 1  # radius of my circle
-circum = 2 * np.pi * x
-print("Radius of my circle is ", r, "and circumference is ", circum)
-print()
+def circumference(r):
+    circum = 2 * np.pi * r
+    return circum
 
-SurfArea = np.pi * r**2
-print("Surface area is ", SurfArea)
-print()
+def SurfArea(r):
+    SA = np.pi * r**2
+    return SA
+
