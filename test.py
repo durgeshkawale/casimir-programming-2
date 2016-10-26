d67be1a3322ca4a703bd0e3ce10978a0ae214173
Durgesh Kawale <durgeshkawale@gmail.com>
2016-10-26 09:12:11 +0000

    surface area calculator

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 print('hello world')
 import numpy as np
-x = 1  # radius of my circle
-y = 2 * np.pi * x
-print("Radius of my circle is ", x, "and circumference is ", y)
+r = 1  # radius of my circle
+circum = 2 * np.pi * x
+print("Radius of my circle is ", r, "and circumference is ", circum)
+print()
+
+SurfArea = np.pi * r**2
+print("Surface area is ", SurfArea)
 print()
